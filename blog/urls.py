de5925fe4eb259fdf7d3f36b5372ba3f7ab363c8
Artem_Svistelnik <artem.svistelnik@gmail.com>
2021-01-09 11:47:56 +0200

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import  views
+
+app_name='blog'
+urlpatterns=[
+    path('',views.post_list,
+         name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/'
+         '<slug:post>/',views.post_detail,
+         name='post_detail')
+]
+# templates/blog/post
+# в папке blog создать base.html
+# в папке post создать list.html и
+#                      detail.html
