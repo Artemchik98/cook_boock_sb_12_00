4395ffa30b7f90df221aa7122bb6a87f909c084e
Artem_Svistelnik <artem.svistelnik@gmail.com>
2021-01-23 11:09:41 +0200

    lesson 16.01

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render,get_object_or_404
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 from .models import Post ,PostPoint
 def post_list(request):
-    posts=Post.objects.all()
-    return  render(request,
-           'blog/post/list.html',
-           {'posts':posts})
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 1)  # Выводи по одному обьекту на страницу
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
+
+
 
 def post_detail(request,year,
                 month,day,post):
